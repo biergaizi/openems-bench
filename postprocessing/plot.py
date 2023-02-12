d93b98c9611c4b7a6b704b1285c989de2e70f88e
Yifeng Li <tomli@tomli.me>
2023-02-12 14:44:03 +0000

    plot.py: improve plotting.

--- postprocessing/plot.py
+++ postprocessing/plot.py
@@ -75,7 +75,6 @@ def plot():
         else:
             assert False
 
-    con.commit()
     con.close()
 
     # plot
@@ -104,11 +103,100 @@ def plot():
     ax.bar_label(group_patched, padding=3, labels=y_patched_label, linespacing=0.85)
     ax.set_yticks(x, x_benchmark)
     ax.set_xlim(0, max(y_patched) * 1.4)
+    ax.set_title(machine)
     ax.legend()
     fig.tight_layout()
     plt.show()
 
-if sys.argv[1] == "list":
-    list_machine()
-elif sys.argv[1] == "plot":
-    plot()
+def versus(branch, machine_a, machine_b):
+    con = sqlite3.connect(DB_FILE)
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+
+    x_benchmark = set()
+    y_macha = []
+    y_macha_thread = []
+    y_machb = []
+    y_machb_thread = []
+
+    benchmark_list = list(map(
+        lambda x: x['script'],
+        cur.execute(
+            "SELECT DISTINCT script FROM benchmarks "
+            "WHERE hostname=? AND user=? "
+            "INTERSECT "
+            "SELECT DISTINCT script FROM benchmarks "
+            "WHERE hostname=? AND user=?;",
+            (machine_a, branch, machine_b, branch)
+        ).fetchall()
+    ))
+    x_benchmark = benchmark_list
+
+    row_list = cur.execute(
+        "SELECT hostname, script, user, trial, threads, max(speed) FROM benchmarks "
+        "WHERE user=? AND (hostname=? OR hostname=?)"
+        "GROUP BY script, hostname "
+        "ORDER BY script;",
+        (branch, machine_a, machine_b)
+    )
+
+    for row in row_list:
+        if row["script"] not in x_benchmark:
+            continue
+
+        if row["hostname"] == machine_a:
+            y_macha_thread.append(row["threads"])
+            y_macha.append(row["max(speed)"])
+        elif row["hostname"] == machine_b:
+            y_machb_thread.append(row["threads"])
+            y_machb.append(row["max(speed)"])
+        else:
+            assert False
+
+    con.close()
+
+    # plot
+    print(x_benchmark)
+    print(y_macha)
+    print(y_machb)
+
+    y_macha_label = []
+    for i in range(len(y_macha)):
+        y_macha_label += ["%.2f\n%d threads" % (y_macha[i], y_macha_thread[i])]
+
+    y_machb_label = []
+    for i in range(len(y_machb)):
+        speedup = (y_machb[i] / y_macha[i]) * 100
+        y_machb_label += ["%.2f (%d%%)\n%d threads" % (y_machb[i], speedup, y_machb_thread[i])]
+
+    x = np.arange(len(x_benchmark))
+    width = 0.4
+
+    fig, ax = plt.subplots()
+    group_upstream = ax.barh(x - width / 2, y_macha, width, label=machine_a)
+    group_patched = ax.barh(x + width / 2, y_machb, width, label=machine_b)
+    ax.invert_yaxis()
+    ax.set_xlabel('Speed (MC/s)')
+    ax.bar_label(group_upstream, padding=3, labels=y_macha_label, linespacing=0.85)
+    ax.bar_label(group_patched, padding=3, labels=y_machb_label, linespacing=0.85)
+    ax.set_yticks(x, x_benchmark)
+    ax.set_xlim(0, max(y_machb) * 1.4)
+    if branch == "hack":
+        ax.set_title("Optimized Source (experimental)")
+    elif branch == "upstream":
+        ax.set_title("Upstream Source")
+    ax.legend()
+    fig.tight_layout()
+    plt.show()
+
+try:
+    if sys.argv[1] == "list":
+        list_machine()
+    elif sys.argv[1] == "plot":
+        plot()
+    elif sys.argv[1] == "versus":
+        versus(sys.argv[4], sys.argv[2], sys.argv[3])
+    else:
+        print("unknown command", file=sys.stderr)
+except IndexError:
+    print("unknown command", file=sys.stderr)
