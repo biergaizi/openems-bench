601ff0542bb6dc6a89c80b7e427f70933f9cf467
Yifeng Li <tomli@tomli.me>
2023-05-15 20:07:26 +0000

    report.py: update to handle multiple targets for the new benchmark script
    Signed-off-by: Yifeng Li <tomli@tomli.me>

--- run/report.py
+++ run/report.py
@@ -22,13 +22,17 @@ def parse_result(file):
 
 
 hostname = socket.gethostname()
-user = getpass.getuser()
 
-print("Hostname, User, Script, Trial, Threads, Speed")
-for file in chain(os.scandir("openems-python"), os.scandir("openems-octave"), os.scandir("pyems")):
-    for i in range(0, 4):
-        if file.name.endswith(".result.%d" % i):
-            script_name = ".".join([file.name.split(".")[0], file.name.split(".")[1]])
-            trial = file.name.split(".result.")[-1]
-            for threads, speed in parse_result(file):
-                print("%s, %s, %s, %s, %d, %.2f" % (hostname, user, script_name, trial, threads, speed))
+print("Hostname, Target, Script, Trial, Threads, Speed")
+for target in os.scandir("./result"):
+    for file in chain(
+            os.scandir(target.path + "/openems-python"),
+            os.scandir(target.path + "/openems-octave"),
+            os.scandir(target.path + "/pyems")
+    ):
+        for i in range(0, 4):
+            if file.name.endswith(".result.%d" % i):
+                script_name = ".".join([file.name.split(".")[0], file.name.split(".")[1]])
+                trial = file.name.split(".result.")[-1]
+                for threads, speed in parse_result(file):
+                    print("%s, %s, %s, %s, %d, %.2f" % (hostname, target.name, script_name, trial, threads, speed))
