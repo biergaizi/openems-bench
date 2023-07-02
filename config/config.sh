80baca9c5a621fa87cd3084343efe1b61b05e03d
Yifeng Li <tomli@tomli.me>
2023-07-02 02:46:20 +0000

    config.sh: tiling test
    Signed-off-by: Yifeng Li <tomli@tomli.me>

--- config/config.sh
+++ config/config.sh
@@ -4,42 +4,24 @@
 
 # Edit these lines to change the tested number of threads
 export BENCH_MIN_THREAD=1
-export BENCH_MAX_THREAD=4
+export BENCH_MAX_THREAD=16
 
 # Edit this list to change benchmarked targets
 ENABLED_LIST=(
-    "upstream"
-    "xyzn"
-    "nxyz"
+    "tiling"
 )
 
 CFLAGS_LIST=(
     "o3"                        # alias, no space allowed
     "-pipe -O3"                 # flag
     EOF
-
-    "native"                    # alias, no space allowed
-    "-pipe -O3 -march=native"   # flag
-    EOF
 )
 
 # A list of all available targets
 REPO_LIST=(
-    "upstream"                                           # alias
+    "tiling"                                             # alias
     "https://github.com/thliebig/openEMS-Project.git"    # bundle repo
     "https://github.com/biergaizi/openEMS.git"           # openEMS individual repo
-    "preserve-cflags"                                    # openEMS individual branch
-    EOF
-
-    "xyzn"                                               # alias
-    "https://github.com/thliebig/openEMS-Project.git"    # bundle repo
-    "https://github.com/biergaizi/openEMS.git"           # openEMS individual repo
-    "rework-stage3-corrected-xyzn"                       # openEMS individual branch
-    EOF
-    
-    "nxyz"                                               # alias
-    "https://github.com/thliebig/openEMS-Project.git"    # bundle repo
-    "https://github.com/biergaizi/openEMS.git"           # openEMS individual repo
-    "rework-stage3-corrected-nxyz-pad"                   # openEMS individual branch
+    "project-diamond-rework1"                            # openEMS individual branch
     EOF
 )
